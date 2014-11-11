8965dabd997565d71d3e0a84350ea1f34f847dc6
May Yang <mayy.yang@gmail.com>
2014-11-11 13:41:26 -0800

    Initial Commit

---
+++ LostCharactersChallenge/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  LostCharactersChallenge
+//
+//  Created by May Yang on 11/11/14.
+//  Copyright (c) 2014 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
